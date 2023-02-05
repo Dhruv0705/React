55c5b76d043fab51e374b59d126015ac70052eb8
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-02-05 11:55:16 -0600

    Update UTube App

--- utube_app/src/components/SearchBar.jsx
+++ utube_app/src/components/SearchBar.jsx
@@ -18,24 +18,24 @@ const SearchBar = () => {
   };
 
   return (
-    <Paper
-      component='form'
-      onSubmit={onHandleSubmit}
-      sx={{
-        borderRadius: 20,
-        border: '1px solid #e3e3e3',
-        pl: 2,
-        boxShadow: 'none',
-        mr: { sm: 5 },
-      }}
+    <Paper 
+        component='form' 
+        onSubmit={onHandleSubmit} 
+        sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: { sm: 5 }, }}
     >
-      <input
-        className='search-bar'
-        placeholder='Search...'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <input 
+            className='search-bar' 
+            placeholder='Search...' 
+            value={searchTerm} 
+            onChange={(e) => setSearchTerm(e.target.value)} 
+            sx={{ color: 'red' }} 
+        />
+            <IconButton 
+                IconButton 
+                type='submit' 
+                sx={{ p: '10px', color: 'red' }} 
+                aria-label='search'
+            >
         <SearchIcon />
       </IconButton>
     </Paper>
