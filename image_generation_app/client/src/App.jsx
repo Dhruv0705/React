9d8f6986edc64a100df7718013fc0c6e9e92ead3
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-02-25 15:36:58 -0600

    Finish Image App

--- image_generation_app/client/src/App.jsx
+++ image_generation_app/client/src/App.jsx
@@ -23,12 +23,10 @@ const App = () => {
       </header>
 
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
-        
         <Routes>
           <Route path= "/" element= {<HomePage/>}/>
           <Route path= "/create-post" element= {<CreatePostPage/>}/>
         </Routes>
-
       </main>
 
     </BrowserRouter>
--- image_generation_app/server/routes/dalleRoutes.js
+++ image_generation_app/server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_SECRET_API_KEY,
+  apiKey: process.env.OPENAI_API_SECRET,
 });
 
 const openai = new OpenAIApi(configuration);
